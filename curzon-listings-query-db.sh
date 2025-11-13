f98eec5a3dc29378830893115394190acb1388d9
Ian Miell <ian.miell@gmail.com>
2025-11-13 15:13:57 +0000

    latest

--- curzon-listings-query-db.sh
+++ curzon-listings-query-db.sh
@@ -4,6 +4,7 @@ cd $(dirname ${BASH_SOURCE[0]})
 
 DBFILE=curzon-showtimes.db
 
+(
 echo "BY FILM"
 /home/linuxbrew/.linuxbrew/bin/sqlite3 "${DBFILE}" -cmd ".headers off" -cmd ".mode list" "$LOCATION_CTE
 SELECT
@@ -23,3 +24,4 @@ SELECT
    JOIN location l ON l.code  = fs.location_code
    GROUP BY l.code
    ORDER BY l.code, min(fs.starts_at);" | column -t -s '|'
+) > curzon_listings.txt
